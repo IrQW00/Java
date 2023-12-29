635c802772479466abac093c29d7d55fbe26c20e
Ира Нефагина <121683926+IrQW00@users.noreply.github.com>
2023-12-29 18:02:57 +0500

    Add files via upload

---
+++ buns/mod8/mod8/src/test/java/com/mod8/spring/springboot/spring_course_springboot/Mod8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mod8.spring.springboot.spring_course_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
